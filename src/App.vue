2402703a7c6c03fe1fc2012719dcda0c84452dc5
Max Gabrielsson <max.gabrielsson@squeed.com>
2019-11-30 19:06:56 +0100

    Styling

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,54 @@
 <template>
-  <div id="app" >
+    <div id="app">
 
 
-    <Ingredients :msg="info"></Ingredients>
-    <Menu :menu="menu"></Menu>
-  </div>
+        <Ingredients :msg="info"></Ingredients>
+        <Menu :menu="menu"></Menu>
+    </div>
 </template>
 
 <script>
-import Ingredients from "./components/Ingredients";
-import Menu from "./components/Menu";
+    import Ingredients from "./components/Ingredients";
+    import Menu from "./components/Menu";
 
-export default {
-  name: "app",
-  components: {
-    Ingredients,
-    Menu
-  },
-  data() {
-    return {
-      info: [],
-      menu: []
+    export default {
+        name: "app",
+        components: {
+            Ingredients,
+            Menu
+        },
+        data() {
+            return {
+                info: [],
+                menu: []
+            };
+        },
+        mounted() {
+
+        },
+        methods: {},
     };
-  },
-  mounted() {
-    
-  },
-  methods: {
-   
-  },
-};
 </script>
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: wheat;
-  margin-top: 60px;
-  background-color: black;
-  opacity: 0.7;  
-  
+    #app {
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: wheat;
+        padding-top: 60px;
+        opacity: 0.85;
+        font-family: 'Courier Next', courier, monospace;
+        background-color: black;
+        background-size: 100%;
 
-}
+    }
 
-html {
-  background-size: cover;
-}
+    html {
+        background-image: url('assets/pizza.jpg');
+        background-position: bottom;
+        background-attachment: fixed;
+        background-size: cover;
+    }
 </style>
  
